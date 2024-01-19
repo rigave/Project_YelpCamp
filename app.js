81266607ff09071ee98eba8668fc6a600b820f91
rohan igave <rigave200@gmail.com>
2024-01-19 16:40:20 +0530

    fix db error

--- app.js
+++ app.js
@@ -25,9 +25,7 @@ const reviewRoutes = require("./routes/reviews");
 const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp';
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
-    useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
 })
 
 const db = mongoose.connection;
